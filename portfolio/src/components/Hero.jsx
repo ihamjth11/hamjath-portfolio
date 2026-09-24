b2b4b52745bf5c1707e354dad7cfe0325ca2b849
Hamjath <m.hamjath11@gmail.com>
2026-09-24 10:21:14 +0530

    Add Open Graph meta tags and preview banner for link sharing

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -123,7 +123,7 @@ export default function Hero() {
           ["#projects", "Projects"],
           ["#contact", "Contact"],
         ].map(([href, label]) => {
-          return (
+          return (<a
             
               key={href}
               href={href}
